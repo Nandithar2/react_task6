5de4de6feeed110eebcc72f30c8cbff5f2aaf12d
unknown <lakshminanditha123@gmail.com>
2023-10-14 18:37:29 +0530

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <p>Email: your.email@example.com</p>
+      <p>LinkedIn: linkedin.com/in/yourprofile</p>
+      <p>GitHub: github.com/yourusername</p>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Your Name</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
